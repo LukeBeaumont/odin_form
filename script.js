4bae31443acddcf5cae0d6e8c9e6629a7bb72cea
LukeBeaumont <lbeaumont50@msn.com>
2023-05-02 20:16:57 +0100

    intial script commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll(".inputs");
+
+inputs.forEach(input => input.addEventListener("click",()=> {
+    
+        input.classList.add("inputs:hover label");
+   
+    }
+))
+
